a74e0dbe883d648d19574cf61cf599519e90f05e
Partithura <jorgebrunoalmeidadossantos@gmail.com>
2023-02-22 10:18:03 -0300

    ✨ Tabela exibindo os dados
    dados puxados da fake api são exibidos no q-table

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,52 @@
 <template>
-  <q-page padding> </q-page>
+  <q-page padding>
+    <q-table title="Treats" :rows="posts" :columns="columns" row-key="name" />
+  </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
+import { api } from "boot/axios";
 
 export default defineComponent({
   name: "IndexPage",
+  setup() {
+    const posts = ref([]);
+    const columns = [
+      { name: "id", field: "id", label: "Id", sortable: true, align: "left" },
+      {
+        name: "title",
+        field: "title",
+        label: "Título",
+        sortable: true,
+        align: "left",
+      },
+      {
+        name: "author",
+        field: "author",
+        label: "Autor",
+        sortable: true,
+        align: "left",
+      },
+    ];
+
+    onMounted(() => {
+      getPosts();
+    });
+
+    const getPosts = async () => {
+      try {
+        const { data } = await api.get("posts");
+        posts.value = data;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    return {
+      posts,
+      columns,
+    };
+  },
 });
 </script>
